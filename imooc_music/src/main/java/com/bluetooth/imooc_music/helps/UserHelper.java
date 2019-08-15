d9f2a5634ad7a1c3e4890241a7063ef34a2c51b6
yifeiguorenyu <623301493@qq.com>
2019-08-15 14:11:55 +0800

    14:11  08-15
    Signed-off-by: yifeiguorenyu <623301493@qq.com>

---
+++ imooc_music/src/main/java/com/bluetooth/imooc_music/helps/UserHelper.java
@@ -0,0 +1,15 @@
+package com.bluetooth.imooc_music.helps;
+
+import android.content.SharedPreferences;
+
+/**
+ * 1 用户登录
+ *      1 当用户没有登录过应用程序时 利用SharedPreferences 保存用户登录的标记,
+ *      2 利用全局单例类UserHelper  保存用户登录信息
+ *
+ * 2 用户退出
+ */
+public class UserHelper {
+    public void record(){
+    }
+}
